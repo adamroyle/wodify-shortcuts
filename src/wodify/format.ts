7d5fb63a8f94c0ffa59f47489587b975d9d7dc06
Adam Royle <adam@bigfish.tv>
2022-10-02 09:38:51 +1000

    Format primary workouts better

--- src/wodify/format.ts
+++ src/wodify/format.ts
@@ -22,21 +22,44 @@ export function formatWorkout(workoutComponents: WorkoutComponent[]): string {
     .filter(excludeEmptySections)
     .map(formatWorkoutComponent)
     .join('\n\n')
+    .replace(/\r/g, '') // remove carriage returns
+    .replace(/[\t ]+$/gm, '') // remove trailing whitespace
+    .replace(/\n\n\n+/g, '\n\n') // remove extra newlines
 }
 
 function formatWorkoutComponent(component: WorkoutComponent): string {
   if (component.IsSection) {
-    return component.Name
+    return component.Name.trim().toLocaleUpperCase()
   }
   return [
-    [component.Name, component.Description].filter(Boolean).join('\n'),
+    [
+      cleanWorkoutName(component.Name, component.Description.trim() || component.Comment.trim()),
+      removeFillerText(component.Description),
+    ]
+      .filter(Boolean)
+      .join('\n'),
     component.TotalWeightLiftingComponents.List.join('\n'),
-    component.Comment,
+    removeFillerText(component.Comment.trim()),
   ]
     .filter(Boolean)
     .join('\n\n')
 }
 
 function excludeEmptySections(c: WorkoutComponent, i: number, arr: WorkoutComponent[]) {
-  return !(c.IsSection && arr[i + 1]?.IsSection)
+  return !(c.IsSection && (i + 1 === arr.length || arr[i + 1].IsSection))
+}
+
+function cleanWorkoutName(name: string, description: string): string {
+  name = name.trim()
+  if (name.toLowerCase() == 'metcon') {
+    return ''
+  }
+  if (description.toLocaleLowerCase().startsWith(name.toLocaleLowerCase())) {
+    return ''
+  }
+  return name
+}
+
+function removeFillerText(text: string): string {
+  return text.replace(/^(Athlete Instructions|Instructions|Athlete Notes|Extra Details)$/gim, '')
 }
