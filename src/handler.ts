9163c77fd08eeaf5453975714a1b06a55e1ea579
Adam Royle <adam@bigfish.tv>
2022-10-02 15:06:46 +1000

    Log a hash of email+password instead of the email directly.

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,5 @@
 import type { Handler, APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda'
+import crypto from 'crypto'
 
 import { getWorkout, signin } from './functions.js'
 
@@ -11,7 +12,7 @@ export const getWorkoutHandler: Handler<APIGatewayProxyEventV2, APIGatewayProxyR
   const excludeSections = formData.getAll('exclude_section').filter(Boolean)
 
   if (!date || !email || !password) {
-    console.error('Date, email and password are required.', { date, email })
+    console.error('Date, email and password are required.')
     return {
       statusCode: 400,
       body: 'Date, email and password are required.',
@@ -24,7 +25,7 @@ export const getWorkoutHandler: Handler<APIGatewayProxyEventV2, APIGatewayProxyR
       body: await getWorkout({ username: email, password, date, includeSections, excludeSections }),
     }
   } catch (error: any) {
-    console.error(error.message, { email, date })
+    console.error(error.message, { user: hash(email, password), date, includeSections, excludeSections })
     return {
       statusCode: 500,
       body: error.message,
@@ -46,7 +47,7 @@ export const signinHandler: Handler<APIGatewayProxyEventV2, APIGatewayProxyResul
   }
 
   if (!date || !email || !password) {
-    console.error('Date, email and password are required.', { date, email })
+    console.error('Date, email and password are required.')
     return {
       statusCode: 400,
       body: 'Date, email and password are required.',
@@ -55,16 +56,20 @@ export const signinHandler: Handler<APIGatewayProxyEventV2, APIGatewayProxyResul
 
   try {
     const message = await signin({ username: email, password, date, includeClasses, excludeClasses })
-    console.log(message, { email, date, includeClasses, excludeClasses })
+    console.log(message, { user: hash(email, password), date, includeClasses, excludeClasses })
     return {
       statusCode: 200,
       body: message,
     }
   } catch (error: any) {
-    console.error(error.message, { email, date, includeClasses, excludeClasses })
+    console.error(error.message, { user: hash(email, password), date, includeClasses, excludeClasses })
     return {
       statusCode: 500,
       body: error.message,
     }
   }
+}
+
+function hash(email: string, password: string): string {
+  return crypto.createHash('md5').update(`${email}:${password}`).digest('hex')
 }
