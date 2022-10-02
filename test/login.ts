0e08db38847ed7330a6a6c4c8075c8f8e5d43a6a
Adam Royle <adam@bigfish.tv>
2022-10-02 12:33:50 +1000

    Remove unused test scripts

--- test/login.ts
+++ test/login.ts
@@ -1,28 +1,12 @@
 import { login, listClasses } from '../src/wodify/api'
 
-async function main(username: string, password: string, date: string, classId: string) {
+async function main(username: string, password: string, date: string) {
   const start = Date.now()
 
   try {
     const session = await login(username, password)
-    // const programs = await listPrograms(session)
     const classes = await listClasses(session, date)
-    // console.log(programs)
     console.log(classes)
-    // const workout = await listWorkoutComponents(session, date)
-    // console.log(formatWorkout(getPrimaryWorkout(workout)) || 'No workout found')
-    // console.log(workout)
-
-    // const programs = await listPrograms(session)
-    // const classes = await listClasses(session, date)
-
-    // console.log(programs)
-    // console.log(classes)
-    // const workout = await listWorkoutComponents(session, '2022-08-04')
-    // const thing = await signinClass(session, classId)
-    // console.log(thing)
-    // const classAccess = await getClassAccess(session, classId)
-    // console.log(classAccess)
   } catch (e: any) {
     console.log('Error:', e.message)
   }
@@ -30,4 +14,4 @@ async function main(username: string, password: string, date: string, classId: s
   console.log('Took', Date.now() - start, 'ms')
 }
 
-main(process.argv[2], process.argv[3], process.argv[4], process.argv[5])
+main(process.argv[2], process.argv[3], process.argv[4])
--- test/test.ts
+++
@@ -1,9 +0,0 @@
-import { preloadApiCache } from '../src/wodify/network'
-
-async function main() {
-  const start = Date.now()
-  console.log(await preloadApiCache())
-  console.log('Took', Date.now() - start, 'ms')
-}
-
-main()
