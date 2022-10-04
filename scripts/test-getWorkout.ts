23f3948219e498ee25d9c119c494e9c994271b8f
Adam Royle <adam@bigfish.tv>
2022-10-04 17:31:30 +1000

    Add test-getWorkout script

---
+++ scripts/test-getWorkout.ts
@@ -0,0 +1,16 @@
+import { getWorkout } from '../src/functions'
+
+async function main(username: string, password: string, date: string) {
+  const start = Date.now()
+
+  try {
+    const workout = await getWorkout({ username, password, date, includeSections: [], excludeSections: [] })
+    console.log(workout)
+  } catch (e: any) {
+    console.log('Error:', e.message)
+  }
+
+  console.log('Took', Date.now() - start, 'ms')
+}
+
+main(process.argv[2], process.argv[3], process.argv[4])
