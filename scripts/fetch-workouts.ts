9758cb2ff3ec9f68540bc9cf2f2bb2ff198ecab9
Adam Royle <adam@bigfish.tv>
2022-10-05 14:53:06 +1000

    Update fetch workouts to include the next 7 days. Also save in the right dir

--- scripts/fetch-workouts.ts
+++ scripts/fetch-workouts.ts
@@ -5,9 +5,10 @@ async function main(username: string, password: string) {
   const session = await login(username, password)
   // iterate each day from 1 month ago
   const today = new Date()
+  const nextWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7)
   const oneMonthAgo = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate())
   const days = []
-  for (let d = oneMonthAgo; d <= today; d.setDate(d.getDate() + 1)) {
+  for (let d = oneMonthAgo; d <= nextWeek; d.setDate(d.getDate() + 1)) {
     days.push(new Date(d))
   }
   // fetch workouts
@@ -15,7 +16,7 @@ async function main(username: string, password: string) {
     const date = day.toISOString().split('T')[0]
     const workout = await listWorkoutComponents(session, date)
     if (workout.length) {
-      fs.writeFileSync(`test/workouts/${date}.json`, JSON.stringify(workout, null, 2))
+      fs.writeFileSync(`scripts/workouts/${date}.json`, JSON.stringify(workout, null, 2))
     }
   }
 }
