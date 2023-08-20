b0ee4b9831076eeb8344c05e5f8c0f54ff1e162a
Adam Royle <adam@bigfish.tv>
2023-08-20 17:24:10 +1000

    Batch fetching workouts, and specify program id for crossaxed

--- scripts/fetch-workouts.ts
+++ scripts/fetch-workouts.ts
@@ -3,6 +3,7 @@ import { listWorkoutComponents, login } from '../src/wodify/api'
 
 async function main(username: string, password: string) {
   const session = await login(username, password)
+  const programId = session.User.GymProgramId === '20714' ? '109084' : session.User.GymProgramId
   // iterate each day from 1 month ago
   const today = new Date()
   const nextWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7)
@@ -12,6 +13,7 @@ async function main(username: string, password: string) {
     days.push(new Date(d))
   }
   // fetch workouts
+  let requestCount = 0
   for (const day of days) {
     const date = day.toISOString().split('T')[0]
     const filename = `scripts/workouts/${date}.json`
@@ -19,9 +21,20 @@ async function main(username: string, password: string) {
       console.log(`Skipping ${date} (already exists)`)
       continue
     }
-    const workout = await listWorkoutComponents(session, date)
-    if (workout.length) {
-      fs.writeFileSync(filename, JSON.stringify(workout, null, 2))
+    requestCount++
+    listWorkoutComponents(session, date, programId)
+      .then((workout) => {
+        if (workout.length) {
+          fs.writeFileSync(filename, JSON.stringify(workout, null, 2))
+        }
+        requestCount--
+      })
+      .catch((err) => {
+        console.error(err)
+        requestCount--
+      })
+    while (requestCount >= 5) {
+      await new Promise((resolve) => setTimeout(resolve, 100))
     }
   }
 }
